0cfaea796c9971b330e5790942f2ad8920d60b97
Gabriel Muniz <gabrielmfd@gmail.com>
2023-10-11 10:35:33 -0300

    Feat: cria types.ts

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,15 @@
+import { useState } from 'react';
+
 const Home = () => {
+  const [data, setData] = useState();
+
+  const getFetch = async () => {
+    const response = fetch('https://servicodados.ibge.gov.br/api/v3/noticias/?qtd=100');
+    const resolved = (await response).json();
+    resolved.then((info) => setData(info));    
+  }
+  
+  console.log(data);
   return (
     <h1>Home</h1>
   )
---
+++ src/types.ts
@@ -0,0 +1,23 @@
+export type IBGE_Api = {
+  count: number,
+  items: IBGE_News[],
+  page: number,
+  previousPage: number,
+  showingFrom: number,
+  showingTo: number,
+  totalPages: number,
+}
+
+export type IBGE_News = {
+  data_publicacao: string,
+  destaque: boolean,
+  editorias: string,
+  id: number,
+  imagens: string,
+  introducao: string,
+  link: string,
+  produto_id: number,
+  produtos: string,
+  tipo: string,
+  titulo: string,
+}
